496a46e80d0e65fe457d14fdbf2820819da3eb87
lequanglocdev <quanglocdev@gmail.com>
2024-11-11 10:19:54 +0700

    create project FE

---
+++ estate-insurance-fe/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "@material-tailwind/react";
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello FE</h1>
+        <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
