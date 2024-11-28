8fe19a6864ec73b3bc0fc1585ffae7fe0d5c6d53
Worty76 <lethanhdat762003@gmail.com>
2024-11-28 10:41:45 +0700

    feat: add contact and about page

--- estate-insurance-fe/src/App.js
+++ estate-insurance-fe/src/App.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { SignIn } from "./pages/Auth";
 import { Home } from "./pages/Home/Home";
+import { AboutUs } from "./pages/About";
+import { ContactUs } from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/login" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/insurance-types" element={<></>} />
       </Routes>
     </div>
   );
